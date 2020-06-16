3ef0766118cf5a54f91905cb5185db5ee663996f
Billy <45759837+Jon3sjns@users.noreply.github.com>
2020-06-16 16:46:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $var=json_decode(get("https://api.quran.sutanlab.id/surah/"),true);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <title>Qur'an</title>
+    <title>Al-qur'an</title>
     <meta name="description" content="Qur'an">
     <meta name="keywords" content="qur'an">
     <meta name="author" content="Billy">
